712203acca0ebab686fb372e8ee270c388158963
whitealcea <44871864+whitealcea@users.noreply.github.com>
2020-03-20 02:19:21 +0900

    add HelloWorld

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello world.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
