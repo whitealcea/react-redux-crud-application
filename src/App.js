58c95e6e58d7db95bbe2d178a2cb89dada4483ec
whitealcea <44871864+whitealcea@users.noreply.github.com>
2020-03-23 12:41:01 +0900

    learn component props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
-// function App() {
-// return (
-//   <React.Fragment>
-//     <label htmlFor="bar">bar</label>
-//     <input type="text" onChange={()=>{console.log("I am clicked.")}} />
-//   </React.Fragment>
-//   )
-// }
-
 const App = () => {
-  return(
+  const profiles = [
+    {name : "Taro", age : 10}
+    , {name : "Hanako", age : 5}
+    , {name : "NoName"}
+  ]
+  return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, Iam {props.name}!, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age : 1
 }
 
 export default App;
