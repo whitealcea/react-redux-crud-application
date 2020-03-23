b170b7596f543bc23ca166751fe6d5c14765fc1e
whitealcea <44871864+whitealcea@users.noreply.github.com>
2020-03-23 13:16:14 +0900

    implement action and acton creator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ const App = () => (<Counter></Counter>)
 
 class Counter extends Component {
   constructor(props) {
-      super(props)
-      this.state = {count: 0}
+    super(props)
+    this.state = {count: 0}
   }
 
   handlePlusButton = () => {
