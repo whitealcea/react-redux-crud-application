4c80bcb2690499defe698335f32a8b8720fcaf5b
whitealcea <44871864+whitealcea@users.noreply.github.com>
2020-04-08 17:14:58 +0900

    implements events update

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
 import {
+  CREATE_EVENT,
   READ_EVENTS,
+  READ_EVENT,
+  UPDATE_EVENT,
   DELETE_EVENT,
 } from '../actions'
 
@@ -8,6 +11,11 @@ export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data
+      return {...events, [data.id]:data}
     case DELETE_EVENT:
       delete events[action.id]
       return {...events}
